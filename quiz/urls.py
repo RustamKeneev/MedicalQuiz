f22f9267d6e2a89e8588ad607b68ef07eb7f807e
rustamkeneev <dr.rustamkeneev@gmail.com>
2021-03-18 00:35:01 +0600

    update project static root

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .import views
+from django.conf.urls.static import static
+from django.conf import settings
 from .serializers import QuizSerializer
 
 urlpatterns = [
@@ -7,4 +9,5 @@ urlpatterns = [
     path('api/quiz/', views.get_all_quizs),
     path('api/quiz/<int:id>/', views.get_options),
     # path("category/",views.CategoryView.as_view()),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+ + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
