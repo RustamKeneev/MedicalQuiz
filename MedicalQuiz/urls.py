1256e812ebad7b262aaad648b93e8f5b13745ad2
rustamkeneev <dr.rustamkeneev@gmail.com>
2021-03-18 10:30:16 +0600

    start jet install

--- MedicalQuiz/urls.py
+++ MedicalQuiz/urls.py
@@ -19,9 +19,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('jet/', include('jet.urls', 'jet')),  # Django JET URLS
     path('admin/', admin.site.urls),
     path("",include("quiz.urls"))
 ]
 
 if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
